611dea6fc4a1bd941bdf129241a57b8dc6e67d75
Kaiwen-d <44253954+Kaiwen-d@users.noreply.github.com>
2018-12-11 20:03:29 +0800

    Add files via upload

--- chat_cmdl_client.py
+++ chat_cmdl_client.py
@@ -1,7 +1,6 @@
 
 from chat_client_class import *
-from audio_server import *
-from chat_utils import *
+
 def main():
     import argparse
     parser = argparse.ArgumentParser(description='chat client argument')
@@ -9,9 +8,6 @@ def main():
     parser.add_argument('--TEST', type=str, default='localhost', help='test IP addr')
     args = parser.parse_args()
     print(args)
-    # aserver = Audio_Server(PORT+1)
-    # aserver.start()
-
     client = Client(args)
 
     client.run_chat()
